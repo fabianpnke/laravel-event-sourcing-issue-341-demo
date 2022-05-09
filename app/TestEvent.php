00a6c551af24be4d081e1a21222ff644db53ff04
Fabian Pankoke <fabian.pankoke@hey.com>
2022-05-09 20:33:12 +0200

    Reproduced Issue #341

---
+++ app/TestEvent.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Spatie\EventSourcing\StoredEvents\ShouldBeStored;
+
+class TestEvent extends ShouldBeStored
+{
+
+}
---
+++ app/ThisWorksAggregateRoot.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Spatie\EventSourcing\AggregateRoots\AggregateRoot;
+
+class ThisWorksAggregateRoot extends AggregateRoot
+{
+    public function doSomething(string $something): self
+    {
+        // Record some events
+        $this->recordThat(new TestEvent());
+
+        // For debugging
+        dump('Called doSomething');
+        logger('Called doSomething');
+
+        return $this;
+    }
+
+    public function otherMethod(string $other): self
+    {
+        // Record some events
+        $this->recordThat(new TestEvent());
+
+        // For debugging
+        dump('This dump should not be here!');
+        logger('This dump should not be here!');
+
+        return $this;
+    }
+}
