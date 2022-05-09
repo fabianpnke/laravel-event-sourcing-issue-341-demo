28e304989bfee250ceb85b0fbf2179d6fe56f97d
Fabian Pankoke <fabian.pankoke@hey.com>
2022-05-09 20:33:12 +0200

    Reproduced Issue #341

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/works', function () {
+    \App\ThisWorksAggregateRoot::retrieve('uuid-1')
+        ->doSomething('something')
+        ->persist();
+
+    return 'DONE!';
+});
+
+Route::get('/fails', function () {
+    \App\ThisFailsAggregateRoot::retrieve('uuid-2')
+        ->doSomething('something')
+        ->persist();
+
+    return 'DONE!';
+});
